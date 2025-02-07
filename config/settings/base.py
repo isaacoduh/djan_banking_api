b1a42ac5130662c8ea8ca994b48f884c6bc1b2f1
Isaac Oduh <ufuomaoduh@gmail.com>
2025-02-07 09:49:39 +0000

    chore: add environment variables

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,11 +40,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "core_apps.user_auth",
-    "core_apps.common",
-    "core_apps.user_profile",
-    "core_apps.accounts",
-    "core_apps.cards",
+    
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
