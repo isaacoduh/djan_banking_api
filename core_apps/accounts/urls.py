e5cb38bee74fa2f520f015ef9a24ecf08ee1088f
Isaac Oduh <ufuomaoduh@gmail.com>
2025-02-21 07:49:41 +0000

    transfers between customerss implementation

--- core_apps/accounts/urls.py
+++ core_apps/accounts/urls.py
@@ -4,6 +4,9 @@ from .views import (
     DepositView,
     InitiateWithdrawalView,
     VerifyUsernameAndWithdrawAPIView,
+    InitiateTransferView,
+    VerifyOTPView,
+    VerifySecurityQuestionView
 )
 
 urlpatterns = [
@@ -19,5 +22,14 @@ urlpatterns = [
         VerifyUsernameAndWithdrawAPIView.as_view(),
         name="verify_username_and_withdraw",
     ),
+    path(
+        "transfer/initiate/", InitiateTransferView.as_view(), name="initiate_transfer"
+    ),
+    path(
+        "transfer/verify-security-question/",
+        VerifySecurityQuestionView.as_view(),
+        name="verify_security_question",
+    ),
+    path("transfer/verify-otp/", VerifyOTPView.as_view(), name="verify_otp"),
 
 ]
